6822a409333f8a6ad57227b639989733d2a86379
gaoyibo <gaoyibo157@gmail.com>
2022-07-14 23:30:54 +0800

    add data_convert sctipt

---
+++ nnunet/run/scipts/data_convert.sh
@@ -0,0 +1,6 @@
+task_name="Task502_FeTA_both"
+task_id="502"
+institution="both"	# both FeTA2022_Institution1 or FeTA2022_Institution2
+
+python -u ../data_convert.py --task_name ${task_name} --institution ${institution}
+nnUNet_plan_and_preprocess -t ${task_id}
